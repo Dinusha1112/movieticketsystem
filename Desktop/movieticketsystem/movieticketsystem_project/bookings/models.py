3e23c7c74da1feec942765ef16a1a5fea1824d7d
Dinusha1112 <dinushasuharshani@gmail.com>
2025-04-11 15:16:05 +0530

    Completed first part: added user profile, movie models, and migrations

--- Desktop/movieticketsystem/movieticketsystem_project/bookings/models.py
+++ Desktop/movieticketsystem/movieticketsystem_project/bookings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    tickets = models.IntegerField()
+
+    def __str__(self):
+        return f"Booking {self.id} for {self.user.username}"
+
 
 # Create your models here.
